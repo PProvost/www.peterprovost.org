94e91e92b79badb3fdbdceccba2540a2426ea2d4
PProvost <peter@provost.org>
2012-10-01 11:00:20 -0600

    Added new responsive YouTube & Vimeo stuff

--- plugins/youtube.rb
+++ plugins/youtube.rb
@@ -1,7 +1,5 @@
 module Jekyll
   class Youtube < Liquid::Tag
-    @@width = 640
-    @@height = 510
 
     def initialize(name, id, tokens)
       super
@@ -9,7 +7,7 @@ module Jekyll
     end
 
     def render(context)
-      %(<iframe width="#{@@width}" height="#{@@height}" src="http://www.youtube.com/embed/#{@id}" frameborder="0" allowfullscreen></iframe>)
+      %(<div class="embed-video-container"><iframe src="http://www.youtube.com/embed/#{@id}"></iframe></div>)
     end
   end
 end
